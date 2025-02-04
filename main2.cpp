b06a2113e87b4127cf458ac8ff21a5572ba810f1
bulandwinoviyanti <bulandwinoviyanti@gmail.com>
2025-02-04 12:37:46 +0700

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+  cout << "==BIODATA DIRI==" << endl;
+  cout << "Nama : Bulan Dwi Noviyanti"<<endl;
+  cout << "Umur : 15"<<endl;
+  cout << "NIS : 24308"<<endl;
+  cout << "NISN : 0084400779"<<endl;
+  cout << "TTL : Semarang 04 November 2008"<<endl;
+  cout << "kelamin : perempuan"<<endl;
+  cout << "agama : islam"<<endl;
+  cout << "status : siswi"<<endl;
+  cout << "anak ke : 2"<<endl;
+  cout << "Alamat : JL.Wologito Barat 8 kelurahan kembangarum kecamatan semarang barat"<<endl;
+  cout << "AsalSMP : smp Nusabhakti"<<endl;
+  cout << "No Telp : 087886006548"<<endl;
+  cout << "Nama Ayah : Sihadi"<<endl;
+  cout << "Nama Ibu : Choerun Ni`mah"<<endl;
+  cout << "Pekerjaan Ayah : Buruh"<<endl;
+  cout << "Pekerjaan Ibu : Ibu Rumah Tangga"<<endl;
+}
+
