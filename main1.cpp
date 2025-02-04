e5e5b90d705340c961830389467facc7d466f3ef
bulandwinoviyanti <bulandwinoviyanti@gmail.com>
2025-02-04 12:37:00 +0700

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{cout << "==BIODATA==" << endl;
+
+string nama;
+int umur;
+int nis;
+string nisn;
+string ttl;
+string kelamin;
+string agama;
+string status;
+int anak;
+string alamat;
+string asalsmp;
+string no;
+string ayah;
+string ibu;
+string pekerjaana;
+string pekerjaani;
+
+// perintah memasukkan
+cout << "masukkan nama anda :" << endl;
+cin >> nama;
+cout << "masukkan usia anda :" << endl;
+cin >> umur;
+cout << "masukkan NIS anda :" << endl;
+cin >> nis;
+cout << "masukkan NISN anda :" << endl;
+cin >> nisn;
+cout << "masukkan Tempat Tanggal Lahir anda :" << endl;
+cin >> ttl;
+cout << "masukkan Jenis Kelamin anda :" << endl;
+cin >> kelamin;
+cout << "masukkan Agama anda :" << endl;
+cin >> agama;
+cout << "masukkan Status anda :" << endl;
+cin >> status;
+cout << "masukkan Anak Ke anda :" <<endl;
+cin >> anak;
+cout << "masukkan Alamat anda :" <<endl;
+cin >> alamat;
+cout << "masukkan Asal SMP :" <<endl;
+cin >> asalsmp;
+cout << "masukkan No Telp anda :" <<endl;
+cin >> no;
+cout << "masukkan Nama Ayah anda :" <<endl;
+cin >> ayah;
+cout << "masukkan Nama Ibu anda :" <<endl;
+cin >> ibu;
+cout << "masukkan Pekerjaan Ayah anda :" <<endl;
+cin >> pekerjaana;
+cout << "masukkan Pekerjaan Ibu anda :" <<endl;
+cin >> pekerjaani;
+
+
+cout << "Nama :" << nama << endl;
+cout << "Umur :" << umur << endl;
+cout << "NIS :" << nis << endl;
+cout << "NISN :" << nis << endl;
+cout << "Tempat Tanggal Lahir :" << ttl << endl;
+cout << "Jenis Kelamin :" << kelamin << endl;
+cout << "Agama :" << agama << endl;
+cout << "Status :" << status << endl;
+cout << "Anak Ke :" << anak << endl;
+cout << "Alamat :" << alamat << endl;
+cout << "Asal SMP :" << asalsmp << endl;
+cout << "No Telp :" << no << endl;
+cout << "Nama Ayah :" << ayah << endl;
+cout << "Nama Ibu :" << ibu << endl;
+cout << "Pekerjaan Ayah :" << pekerjaana << endl;
+cout << "pekerjaan Ibu :" << pekerjaani << endl;
+}
