681b3b12610a5ec97955632d7ebc38c517287e2f
bulandwinoviyanti <bulandwinoviyanti@gmail.com>
2025-02-04 12:32:05 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,74 +3,25 @@
 using namespace std;
 
 int main()
-{cout << "==BIODATA==" << endl;
-
-string nama;
-int umur;
-int nis;
-string nisn;
-string ttl;
-string kelamin;
-string agama;
-string status;
-int anak;
-string alamat;
-string asalsmp;
-string no;
-string ayah;
-string ibu;
-string pekerjaana;
-string pekerjaani;
-
-// perintah memasukkan
-cout << "masukkan nama anda :" << endl;
-cin >> nama;
-cout << "masukkan usia anda :" << endl;
-cin >> umur;
-cout << "masukkan NIS anda :" << endl;
-cin >> nis;
-cout << "masukkan NISN anda :" << endl;
-cin >> nisn;
-cout << "masukkan Tempat Tanggal Lahir anda :" << endl;
-cin >> ttl;
-cout << "masukkan Jenis Kelamin anda :" << endl;
-cin >> kelamin;
-cout << "masukkan Agama anda :" << endl;
-cin >> agama;
-cout << "masukkan Status anda :" << endl;
-cin >> status;
-cout << "masukkan Anak Ke anda :" <<endl;
-cin >> anak;
-cout << "masukkan Alamat anda :" <<endl;
-cin >> alamat;
-cout << "masukkan Asal SMP :" <<endl;
-cin >> asalsmp;
-cout << "masukkan No Telp anda :" <<endl;
-cin >> no;
-cout << "masukkan Nama Ayah anda :" <<endl;
-cin >> ayah;
-cout << "masukkan Nama Ibu anda :" <<endl;
-cin >> ibu;
-cout << "masukkan Pekerjaan Ayah anda :" <<endl;
-cin >> pekerjaana;
-cout << "masukkan Pekerjaan Ibu anda :" <<endl;
-cin >> pekerjaani;
+{
 
 
-cout << "Nama :" << nama << endl;
-cout << "Umur :" << umur << endl;
-cout << "NIS :" << nis << endl;
-cout << "NISN :" << nis << endl;
-cout << "Tempat Tanggal Lahir :" << ttl << endl;
-cout << "Jenis Kelamin :" << kelamin << endl;
-cout << "Agama :" << agama << endl;
-cout << "Status :" << status << endl;
-cout << "Anak Ke :" << anak << endl;
-cout << "Alamat :" << alamat << endl;
-cout << "Asal SMP :" << asalsmp << endl;
-cout << "No Telp :" << no << endl;
-cout << "Nama Ayah :" << ayah << endl;
-cout << "Nama Ibu :" << ibu << endl;
-cout << "Pekerjaan Ayah :" << pekerjaana << endl;
-cout << "pekerjaan Ibu :" << pekerjaani << endl;
+  cout << "==BIODATA DIRI==" << endl;
+  cout << "Nama : Bulan Dwi Noviyanti"<<endl;
+  cout << "Umur : 15"<<endl;
+  cout << "NIS : 24308"<<endl;
+  cout << "NISN : 0084400779"<<endl;
+  cout << "TTL : Semarang 04 November 2008"<<endl;
+  cout << "kelamin : perempuan"<<endl;
+  cout << "agama : islam"<<endl;
+  cout << "status : siswi"<<endl;
+  cout << "anak ke : 2"<<endl;
+  cout << "Alamat : JL.Wologito Barat 8 kelurahan kembangarum kecamatan semarang barat"<<endl;
+  cout << "AsalSMP : smp Nusabhakti"<<endl;
+  cout << "No Telp : 087886006548"<<endl;
+  cout << "Nama Ayah : Sihadi"<<endl;
+  cout << "Nama Ibu : Choerun Ni`mah"<<endl;
+  cout << "Pekerjaan Ayah : Buruh"<<endl;
+  cout << "Pekerjaan Ibu : Ibu Rumah Tangga"<<endl;
 }
+
